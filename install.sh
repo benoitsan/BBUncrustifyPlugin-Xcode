dfb5dffc9f5ff678de72e8aa3235f0072dcfdd43
alexruperez <alexruperez@gmail.com>
2015-01-19 21:17:55 +0100

    Added Script Installation Version

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DOWNLOAD_URI=https://github.com/benoitsan/BBUncrustifyPlugin-Xcode/releases/download/2.0.5/UncrustifyPlugin-2.0.5.zip
+PLUGINS_DIR="${HOME}/Library/Application Support/Developer/Shared/Xcode/Plug-ins"
+
+mkdir -p "${PLUGINS_DIR}"
+cd "${PLUGINS_DIR}"
+curl -SL $DOWNLOAD_URI > UncrustifyPlugin.zip
+unzip -oq UncrustifyPlugin.zip
+rm UncrustifyPlugin.zip
+rm -rf __MACOSX
+
+echo "\nBBUncrustifyPlugin successfuly installed! Please restart your Xcode."
